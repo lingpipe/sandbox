8474ecf4b97bd09bd457ba67b1b8a4ca794f33d3
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2009-06-30 20:36:08 +0000

    basic mesh in place

---
+++ lingmed/trunk/src/com/aliasi/lingmed/mesh/MeshDemoCommand.java
@@ -0,0 +1,50 @@
+package com.aliasi.lingmed.mesh;
+
+import com.aliasi.corpus.ObjectHandler;
+
+import com.aliasi.util.Files;
+import com.aliasi.util.Strings;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+import java.util.zip.GZIPInputStream;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class MeshDemoCommand {
+
+    public static void main(String[] args) throws IOException, SAXException {
+        File meshGzipFile = new File(args[0]);
+        String fileURL = Files.fileToURLName(meshGzipFile);
+        System.out.println("FILE URL=" + fileURL);
+
+        MeshParser parser = new MeshParser();
+        DemoHandler handler = new DemoHandler();
+        parser.setHandler(handler);
+
+        InputStream fileIn = new FileInputStream(meshGzipFile);
+        InputStream gzipIn = new GZIPInputStream(fileIn);
+        InputSource inSource = new InputSource(gzipIn);
+        inSource.setEncoding(Strings.UTF8);
+        
+        parser.parse(inSource);
+        System.out.println("\n===================================================");
+        System.out.println("Final record count=" + handler.mRecordCount);
+    }
+
+    static class DemoHandler implements ObjectHandler<Mesh> {
+        int mRecordCount = 0;
+        public void handle(Mesh mesh) {
+            System.out.println("\n=================================================");
+            System.out.println(mesh);
+            ++mRecordCount;
+        }
+    }
+
+
+
+}
---
+++ lingmed/trunk/src/com/aliasi/lingmed/mesh/MeshParser.java
@@ -0,0 +1,53 @@
+package com.aliasi.lingmed.mesh;
+
+import com.aliasi.corpus.ObjectHandler;
+import com.aliasi.corpus.XMLParser;
+
+import com.aliasi.xml.DelegatingHandler;
+
+import java.io.InputStream;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+
+public class MeshParser extends XMLParser<ObjectHandler<Mesh>> {
+
+
+    protected DefaultHandler getXMLHandler() {
+        return new XMLHandler();
+    }
+    
+    class XMLHandler extends DelegatingHandler {
+        final Mesh.Handler mHandler;
+        XMLHandler() {
+            mHandler = new Mesh.Handler(this);
+            setDelegate(DESCRIPTOR_RECORD_ELEMENT,
+                        mHandler);
+        }
+        @Override
+        public void finishDelegate(String element,
+                                   DefaultHandler handler) {
+            getHandler().handle(mHandler.getMesh());
+        }
+        @Override
+        public InputSource resolveEntity(String publicId, String systemId) 
+            throws SAXException {
+            if (systemId.endsWith("desc2009.dtd")) {
+                InputStream in = this.getClass().getResourceAsStream("/com/aliasi/lingmed/mesh/desc2009.dtd");
+                return new InputSource(in);
+            } 
+            // return super.resolveEntity(publicId,systemId);
+            throw new UnsupportedOperationException("bah");
+        }
+    }
+
+    static final String DESCRIPTOR_RECORD_ELEMENT = "DescriptorRecord";
+
+
+    static final String DESCRIPTOR_CLASS_ATT = "DescriptorClass";
+
+    
+}
