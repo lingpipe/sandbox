33ba28a1b68a3b64963112cd50ac92080d0eda53
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-02-23 04:59:37 +0000

    cleaned up tree

--- rzhetsky/src/edu/uchicago/rzhetsky/cfg/Tree.java
+++ rzhetsky/src/edu/uchicago/rzhetsky/cfg/Tree.java
@@ -5,23 +5,116 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+
+/**
+ * A {@code Tree} represents a complete derivation of a root category
+ * from a sequence of words.  
+ *
+ * <p>All trees have a root category, as returned by {@link
+ * #rootCategory()}.
+ * 
+ * <p>Trees are either lexical or phrasal.  Their type may be
+ * determined with the method {@link #isLexical()}.
+ *
+ * <h3>Lexical Trees</h3>
+ * 
+ * <p>Lexical trees have a word in addition to a root category.
+ * Lexical trees are created with the static factory method 
+ * {@link #createLexical(String,String)}.  For lexical trees,
+ * the method {@link #word()} returns the word.
+ *
+ * <h3>Phrasal Trees</h3>
+ *
+ * <p>Phrasal trees have a sequence of subtrees.  Phrasal trees are
+ * created with the static factory method {@link
+ * #createPhrasal(String,List)}.  For phrasal trees, the method
+ * {@link #subtrees()} returns a view of the list of subtrees.
+ *
+ * @author Bob Carpenter
+ * @version 1.0
+ * @since 1.0
+ */
 public abstract class Tree {
 
-    private final String mMother;
+    private final String mRootCategory;
 
-    public Tree(String mother) {
-        mMother = mother;
+    Tree(String rootCategory) {
+        mRootCategory = rootCategory;
     }
 
-    public String mother() {
-        return mMother;
+    /**
+     * Return the root category for this tree.
+     *
+     * @return the root category.
+     */
+    public String rootCategory() {
+        return mRootCategory;
     }
 
-    public static class Lexical  extends Tree {
+    /**
+     * Return {@code true} if this is a lexical tree.
+     *
+     * @return {@code true} if this is a lexical tree.
+     */
+    public boolean isLexical() {
+        return false;
+    }
+
+    /**
+     * Return the word if this is a lexical tree.
+     *
+     * @return The word for this tree.
+     * @throws UnsupportedOperationException If this is
+     * not a lexical tree.
+     */
+    public String word() {
+        String msg = "Only supported for lexical trees";
+        throw new UnsupportedOperationException(msg);
+    }
+
+    /**
+     * Return the list of subtrees if this is a phrasal tree.
+     *
+     * @return The list of subtrees for this tree.
+     * @throws UnsupportedOperationException If this is not a phrasal
+     * tree.
+     */
+    public List<Tree> subtrees() {
+        String msg = "Only supported for phrasal trees";
+        throw new UnsupportedOperationException(msg);
+    }
+
+    /**
+     * Return a lexical tree with the specified root category
+     * and word.
+     *
+     * @param rootCategory Root for the tree.
+     * @param word Word for the tree.
+     * @return The lexical tree with the specified root and word.
+     */
+    public static Tree createLexical(String rootCategory,
+                                     String word) {
+        return new Lexical(rootCategory,word);
+    }
+
+    /**
+     * Return a phrasal tree with the specified root category
+     * and subtrees.
+     *
+     * @param rootCategory Root category for the tree.
+     * @param subtrees Subtrees for the tree.
+     * @return The phrasal tree with the specified root and subtrees.
+     */
+    public static Tree createPhrasal(String rootCategory,
+                                     List<Tree> subtrees) {
+        return new Phrasal(rootCategory,subtrees);
+    }
+
+    static class Lexical  extends Tree {
         private final String mWord;
-        public Lexical(String mother,
+        public Lexical(String rootCategory,
                        String word) {
-            super(mother);
+            super(rootCategory);
             mWord = word;
         }
         public String word() {
@@ -29,22 +122,22 @@ public abstract class Tree {
         }
         @Override
         public String toString() {
-            return mother() + "[" + word() + "]";
+            return rootCategory() + "[" + word() + "]";
         }
     }
 
-    public static class NonTerminal extends Tree {
-        private final List<Tree> mDaughters;
-        public NonTerminal(String mother,
-                           List<Tree> daughters) {
-            super(mother);
-            mDaughters = new ArrayList<Tree>(daughters);
+    static class Phrasal extends Tree {
+        private final List<Tree> mSubtrees;
+        public Phrasal(String rootCategory,
+                       List<Tree> subtrees) {
+            super(rootCategory);
+            mSubtrees = new ArrayList<Tree>(subtrees);
         }
-        public List<Tree> daughters() {
-            return Collections.unmodifiableList(mDaughters);
+        public List<Tree> subtrees() {
+            return Collections.unmodifiableList(mSubtrees);
         }
         public String toString() {
-            return mother() + "(" + mDaughters + ")";
+            return rootCategory() + "(" + mSubtrees + ")";
         }
     }
 
