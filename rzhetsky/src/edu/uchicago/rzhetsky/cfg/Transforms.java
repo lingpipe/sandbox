dc818aa3dbe23587389189bb2335240f9203fa34
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-02-23 00:35:03 +0000

    grammar and doc going

---
+++ rzhetsky/src/edu/uchicago/rzhetsky/cfg/Transforms.java
@@ -0,0 +1,11 @@
+package edu.uchicago.rzhetsky.cfg;
+
+public class Transforms {
+
+    public static ContextFreeGrammar 
+        unfoldEmptyProductions(ContextFreeGrammar in) {
+        
+        return in;
+    }
+
+}
