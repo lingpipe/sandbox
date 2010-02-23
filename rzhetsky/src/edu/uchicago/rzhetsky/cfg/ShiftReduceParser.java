f5457cf64b9ab9b56a04d26c76bb60d42be41d37
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-02-23 05:45:01 +0000

    shift reduce and other doc, trees to single class with icky type flag

--- rzhetsky/src/edu/uchicago/rzhetsky/cfg/ShiftReduceParser.java
+++ rzhetsky/src/edu/uchicago/rzhetsky/cfg/ShiftReduceParser.java
@@ -14,7 +14,111 @@ import java.util.Set;
 
 /**
  * A {@code ShiftReduceParser} implements the parsing interface
- * using shift-reduce parsig over a specified context-free grammar.
+ * using shift-reduce parsing over a specified context-free grammar.
+ * It returns all valid parses for a given input with respect to
+ * a grammar.
+ *
+ * <h3>Grammar Restrictions</h3>
+ *
+ * <p>Nullary productions (i.e. productions with a zero-length
+ * sequence of daughters) are not allowed.
+ * 
+ * <p>There should not be a sequence of unary productions producing
+ * a cycle.  For instance, the three productions A&rarr;B, B&rarr;C, and
+ * C&rarr;A produce a cycle A&rarr;A.  
+ * 
+ * <h3>Shift-Reduce Parsing</h3>
+ * 
+ * Shift-reduce parsing is a search procedure with a search state
+ * characterized by a list of remaining words to process and
+ * a stack of completed derivations represented as trees.  It
+ * then allows shift and reduce operations to apply to transform
+ * the state.
+ *
+ * <h4>Example</h4>
+ *
+ * Here's an example for a simple parse:
+ *
+ * <blockquote><table border="1" cellpadding="3">
+ * <tr><th>Words</th><th>Subderivations</th><th>Operation</th></tr>
+ * <tr><td>the, kid, ran</td><td></td><td>(n/a)</td></tr>
+ * <tr><td>kid, ran</td><td><b>DET</b>[the]</td><td>shift DET&rarr;the</td></tr>
+ * <tr><td>ran</td><td><b>DET</b>[the], <b>N</b>[kid]</td><td>shift N&rarr;kid</td></tr>
+ * <tr><td>ran</td><td><b>NP</b>[DET[the],N[kid]]</td><td>reduce NP&rarr;DET,N</td></tr>
+ * <tr><td></td><td><b>NP</b>[DET[the],N[kid]], <b>IV</b>[ran]</td><td>shift IV&rarr;ran</td></tr>
+ * <tr><td></td><td><b>NP</b>[DET[the],N[kid]], <b>VP</b>[IV[ran]]</td><td>reduce VP&rarr;IV</td></tr>
+ * <tr><td></td><td><b>S</b>[NP[DET[the],N[kid]],VP[IV[ran]]]</td><td>reduce S&rarr;NP,VP</td></tr>
+ * </table></blockquote>
+ *
+ * <h4>Initialization</h4>
+ *
+ * <p>Shift-reduce parsing starts with the words set to the
+ * sequence of input words and the sequence of subderivations
+ * empty.  For each line other than the first, we provide
+ * an operation, which is either a shift operation involving
+ * a lexical entry or a reduce operation involving a production
+ * rule.
+ *
+ * <h4>Shift Operation</h4>
+ * 
+ * <p>Shift operations remove the first word from the list
+ * of words and add a subderivation consisting of a lexical
+ * tree for that word with a category determined from the
+ * lexcion supplied in the grammar backing the parser.  
+ *
+ * <p>Continuing the above example, the first operation after
+ * initialization shifts the word "the" from the list of words to the
+ * subderivations stack using the lexical entry NP&rarr;the.  The
+ * entry on the list of subderivations is <b>NP</b>[the].  We use
+ * boldface on the roots of the trees in the list of subderivation
+ * trees so that they are easy to distinguish from their derivations.
+ *
+ * <h4>Reduce Operations</h4>
+ * 
+ * <p>Reduce operations use a production rule to remove one
+ * or more categories from the top of the subderivation stack
+ * that match the right-hand side of a production rule and
+ * replace them with a new tree rooted at the mother category
+ * of the production rule and the matching subderivations as
+ * subtrees.  
+ *
+ * <p>Continuing the example, the third operation after shifting
+ * the words "the" and "kid" as DET and N categories, is to
+ * use the production rule NP&rarr;DET,N to reduce the two 
+ * categories to a single noun phrase category.  Here, the
+ * subderivation trees <b>DET</b>[the] and <b>N</b>[kid] are
+ * reduced to a single tree <b>NP</b>[DET[the],N[kid]]
+ * rooted at NP and having the subderivation trees as subtrees.
+ * 
+ * <h4>Completion</h4>
+ *
+ * The parser terminates with a complete parse when there are
+ * no input words in the word list and a single tree on the
+ * derivation stack. 
+ *
+ * <p>Continuing the example, after further shift and reduce
+ * operations, the last line completes the parse of "the,kid,ran" as
+ * the single tree on the subderivation stack.
+ * 
+ * <h4>Non-Deterministic Search</h4>
+ *
+ * For some search states, more than one shift and/or reduce
+ * operation may be applicable.  
+ *
+ * <p>In the above example, in addition to the first reduction
+ * operation reducing the DET and N to an NP, it is also legal to
+ * shift the word ran to an IV.  This IV could then be reduced,
+ * but we would then be left with a stack consisting of DET,N,VP,
+ * which cannot be reduced.  Only the top of the stack (here
+ * written on the right) may be reduced.
+ *
+ * <p>This implementation pursues a reduce-first, longest grammar rule
+ * first strategy.  That is, when there is a conflict between shift
+ * and reduce, reduce is tried first.  If there is a conflict between
+ * two rules, the one reducing more categories is used first.  If
+ * there is a conflict between two production rules with the same
+ * categories or two lexical rules with the same word, they are
+ * pursued in alphabetical order of root category name.
  * 
  * @author Bob Carpenter
  * @version 1.0
@@ -30,11 +134,24 @@ public class ShiftReduceParser extends Parser {
      * context-free grammar.
      *
      * @param cfg Grammar for the parser.
+     * @throws IllegalArgumentException If the grammar contains
+     * a nullary production.
      */
     public ShiftReduceParser(ContextFreeGrammar cfg) {
         super(cfg);
+        verifyNoEmptyProductions(cfg);
         mLexIndex = lexIndex(cfg);
         mRuleIndexRoot = ruleIndex(cfg);
+    }
+
+    static void verifyNoEmptyProductions(ContextFreeGrammar cfg) {
+        for (Production production : cfg.productions()) {
+            if (production.numDaughters() == 0) {
+                String msg = "Shift reduce does not allow empty productions."
+                    + " Found production=" + production;
+                throw new IllegalArgumentException(msg);
+            }
+        }
     }
 
     /**
