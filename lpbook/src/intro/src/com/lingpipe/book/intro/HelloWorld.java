149e6e1a563b7eb1bafd14c10590c83c6d3d99d6
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-06-09 22:33:20 +0000

    getting hello back in

---
+++ lpbook/src/intro/src/com/lingpipe/book/intro/HelloWorld.java
@@ -0,0 +1,11 @@
+/*x HelloWorld.1*/
+package com.lingpipe.book.intro;
+
+public class HelloWorld {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+    }
+
+}
+/*x*/
