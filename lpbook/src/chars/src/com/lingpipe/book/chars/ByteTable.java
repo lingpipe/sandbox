b01facaed2f1a7d34ab8634b8dfac799cb134b9b
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-06-15 20:45:41 +0000

    fancy verbatim, chars and numbers plus code

--- lpbook/src/chars/src/com/lingpipe/book/chars/ByteTable.java
+++ lpbook/src/chars/src/com/lingpipe/book/chars/ByteTable.java
@@ -3,12 +3,22 @@ package com.lingpipe.book.chars;
 public class ByteTable {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 255; ++i) {
-            byte b = 0;
-            b = b | i;
-            System.out.printf("%3d %4d %3d\n",
-                              i, b, Integer.toHexString(i));
+        System.out.println("BASES");
+        System.out.printf("%3s  %5s  %3s  %2s  %8s\n\n",
+                          "10","-10","8","16","2");
+        /*x ByteTable.1*/
+        for (int i = 0; i < 256; ++i) {
+            byte b = (byte) i; // overflows if i > 127
+            int k = b < 0 ? (b + 256) : b;
+            System.out.printf("%3d  %5d  %3s  %2s  %8s\n",
+                              k,
+                              b, 
+                              Integer.toOctalString(k),
+                              Integer.toHexString(k),
+                              Integer.toBinaryString(k));
         }
+        /*x*/
     }
+
 
 }
