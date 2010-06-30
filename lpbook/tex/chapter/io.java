1d23b2e9cdb88adafc8f07586ce8fbd762c706f4
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-06-30 00:04:07 +0000

    started icu

---
+++ lpbook/tex/chapter/io.java
@@ -0,0 +1,24 @@
+\chapter{Input and Output}\label{chap:io}
+
+\firstchar{J}ava provides a rich array of classes and interfaces to
+deal with inputting and outputting data.  Data in this context
+consists of a stream of bytes. These bytes may encode anything, but
+we are particularly interested in the case of natural language text
+data, with or without document structure.  To support streams of
+\code{char} values, Java provides character stream interfaces
+and implementations as well as byte streams.  
+
+In this chapter, we consider Java byte streams and
+character streams as well as identifiers for local content in
+the form of files and arbitrary content in the form of URLs.  We
+will also consider reading to or from arrays, and 
+the use of compression.
+
+
+
+
+
+
+
+
+
