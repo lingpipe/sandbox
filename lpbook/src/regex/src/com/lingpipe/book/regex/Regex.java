1962d8d6423cad792b141b8098664bb1c3922b05
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-07-01 21:03:51 +0000

    juggling

--- lpbook/src/regex/src/com/lingpipe/book/regex/Regex.java
+++ lpbook/src/regex/src/com/lingpipe/book/regex/Regex.java
@@ -3,25 +3,29 @@ package com.lingpipe.book.regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegexTester {
+public class Regex {
 
     public static void main(String[] args) {
+        /*x Regex.1 */
         String regex = args[0];
         String text = args[1];
-        System.out.println("Regex=|" + regex + "|");
+
         Pattern pattern = Pattern.compile(regex);
-
-        System.out.println("Text=|" + text + "|");
-
         Matcher matcher = pattern.matcher(text);
         boolean matches = matcher.matches();
+        /*x*/
+
+        System.out.println("Regex=|" + regex + "|");
+        System.out.println("Text=|" + text + "|");
         System.out.println("Matches=" + matches);
-            
+
+        /*x Regex.2 */
         Matcher finder = pattern.matcher(text);
         while (finder.find()) {
             String found = finder.group();
             int start = finder.start();
             int end = finder.end();
+        /*x*/
             System.out.println("Found |" + found + "|"
                                + " at (" + start + "," + end + ")");
         }
