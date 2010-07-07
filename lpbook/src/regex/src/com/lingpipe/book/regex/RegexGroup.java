92c7e9e69fd9edcb2a4e39982c07d3c2929f352a
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-07-07 22:04:36 +0000

    regex complete, needs proofreading

--- lpbook/src/regex/src/com/lingpipe/book/regex/RegexGroup.java
+++ lpbook/src/regex/src/com/lingpipe/book/regex/RegexGroup.java
@@ -8,13 +8,21 @@ public class RegexGroup {
     public static void main(String[] args) {
         String regex = args[0];
         String text = args[1];
+        System.out.println("regex=|" + regex + "|");
+        System.out.println("text=|" + text + "|");
         /*x RegexGroup.1 */
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
-        for (int i = 0; i < matcher.groupCount(); ++i) {
-            String group = matcher.group(i);
-        /*x*/
-            System.out.printf("Group %2d=|%s|\n",i,group);
+        if (matcher.matches()) {
+            for (int i = 0; i <= matcher.groupCount(); ++i) {
+                String group = matcher.group(i);
+                int start = matcher.start(i);
+                int end = matcher.end(i);
+                /*x*/
+                System.out.printf("Group %2d=|%s| at (%d,%d)\n",i,group,start,end);
+            }
+        } else {
+            System.out.println("No match");
         }
     }
 
