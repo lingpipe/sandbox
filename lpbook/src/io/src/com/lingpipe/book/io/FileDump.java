d4446d382e0189280813ff24d3294b11a07c0c79
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-07-14 23:31:08 +0000

    uris and urls and more on files

--- lpbook/src/io/src/com/lingpipe/book/io/FileDump.java
+++ lpbook/src/io/src/com/lingpipe/book/io/FileDump.java
@@ -7,17 +7,20 @@ public class FileDump {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Available Root Directories on this System");
-        File[] rootDirs = File.listRoots();
-        for (File rootDir : rootDirs)
-            System.out.println("    root dir=" + rootDir);
+        File x = new File("c:/lpb");
+        System.out.println(java.util.Arrays.asList(x.listFiles()));
 
         File foo = new File("foo");
         File fooCaps = new File("Foo");
         File fooAbs = new File("c:\\lpb\\src\\io\\Foo");
 
         File canonFoo = foo.getCanonicalFile();
+        File canonFooCaps = fooCaps.getCanonicalFile();
         File canonFooAbs = fooAbs.getCanonicalFile();
+
+        System.out.println("foo=" + foo + " canonical=" + canonFoo);
+        System.out.println("fooCaps=" + fooCaps + " canonical=" + canonFooCaps);
+        System.out.println("fooAbs=" + fooAbs + " canonical=" + canonFooAbs);
 
         System.out.println("foo equals fooCaps=" + foo.equals(fooCaps));
         System.out.println("foo equals canonFoo=" + foo.equals(canonFoo));
