1d271235cde4e269e8535a57b9db62340a08ee90
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-07-26 21:28:40 +0000

    more tokens

--- lpbook/src/tok/src/com/lingpipe/book/tok/DisplayTokens.java
+++ lpbook/src/tok/src/com/lingpipe/book/tok/DisplayTokens.java
@@ -10,24 +10,27 @@ public class DisplayTokens {
 
     public static void main(String[] args) {
         String text = args[0];
+        /*x DisplayTokens.1 */
         TokenizerFactory tokFact
             = IndoEuropeanTokenizerFactory.INSTANCE;
+        displayTextPositions(text);
         displayTokens(text,tokFact);
+        /*x*/
     }
 
     public static void displayTokens(CharSequence in,
                                      TokenizerFactory tokFact) {
-        displayTextPositions(in);
-        System.out.println();
+        System.out.printf("\n%5s %5s %s\n","START", "END", "TOKEN");
 
+        /*x DisplayTokens.2 */
         char[] cs = Strings.toCharArray(in);
         Tokenizer tokenizer = tokFact.tokenizer(cs,0,cs.length);
 
-        System.out.printf("%5s %5s %s\n","START", "END", "TOKEN");
         for (String token : tokenizer) {
             int start = tokenizer.lastTokenStartPosition();
             int end = tokenizer.lastTokenEndPosition();
-            System.out.printf("%5d %5d %s\n",start,end,"|" + token + "|");
+        /*x*/
+            System.out.printf("%5d %5d  %s\n",start,end,"|" + token + "|");
         }
     }
 
