b5de6f80af5cf060f1ca5094ce92a3baf3b831d9
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-07-27 19:53:50 +0000

    more examples and more writing

---
+++ lpbook/src/io/src/com/lingpipe/book/io/SerializedSingleton.java
@@ -0,0 +1,56 @@
+package com.lingpipe.book.io;
+
+import com.aliasi.util.AbstractExternalizable;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Serializable;
+
+/*x SerializedSingleton.1 */
+public class SerializedSingleton implements Serializable {
+
+    private final String mKey;
+    private final int mValue;
+
+    private SerializedSingleton(String key, int value) {
+        mKey = key;
+        mValue = value;
+    }
+
+    public static final SerializedSingleton INSTANCE
+        = new SerializedSingleton("foo",42);
+/*x*/
+
+/*x SerializedSingleton.2 */
+    Object writeReplace() {
+        return new Externalizer();
+    }
+    
+    static class Externalizer extends AbstractExternalizable {
+
+        public Externalizer() { }
+
+        @Override
+        public Object read(ObjectInput in) {
+            return INSTANCE;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) { }
+    }
+/*x*/
+
+    public static void main(String[] args) 
+        throws IOException, ClassNotFoundException {
+
+        /*x SerializedSingleton.3 */
+        SerializedSingleton s = SerializedSingleton.INSTANCE;
+        Object deser = AbstractExternalizable.serializeDeserialize(s);
+        boolean same = (s == deser);
+        /*x*/
+        System.out.println("Same object=" + same);
+    }
+    
+
+}
