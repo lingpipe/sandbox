b1045edf512f32cc8642f2dc37de546f854cb52c
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-07-31 07:20:57 +0000

    treebank tokenizer basically working

--- lpbook/src/tok/src/com/lingpipe/book/tok/PennTreebankTokenizerFactory.java
+++ lpbook/src/tok/src/com/lingpipe/book/tok/PennTreebankTokenizerFactory.java
@@ -1,5 +1,10 @@
 package com.lingpipe.book.tok;
 
+import com.aliasi.tokenizer.ModifyTokenTokenizerFactory;
+import com.aliasi.tokenizer.RegExTokenizerFactory;
+import com.aliasi.tokenizer.Tokenizer;
+import com.aliasi.tokenizer.TokenizerFactory;
+
 public class PennTreebankTokenizerFactory
     extends ModifyTokenTokenizerFactory {
 
@@ -10,14 +15,14 @@ public class PennTreebankTokenizerFactory
     @Override
     public Tokenizer tokenizer(char[] cs, int start, int len) {
 
-        // replace quotes at begin-of-string and after start
-        // punctuation (Zs,Ps) with open-curly quotes, others with
-        // close curlies, then replace directional punctuation
+        // replace quotes at begin-of-string and after separator or start
+        // punctuation (Z,Ps) with open-curly quotes, others with
+        // close curlies, then replace directional quote punctuation
         // (Pi,Pf) with open and close.
         // does not change length so spans are correct
         String s = new String(cs,start,len)
             .replaceAll("(^\")","\u201C")
-            .replaceAll("(?:[\\p{Zs}\\p{Ps}])\"","\u201C")
+            .replaceAll("(?:[ \\p{Z}\\p{Ps}])\"","\u201C")
             .replaceAll("\"","\u201D")
             .replaceAll("\\{Pi}","\u201C")
             .replaceAll("\\{Pf}","\u201D");
@@ -27,12 +32,12 @@ public class PennTreebankTokenizerFactory
     @Override
     public String modifyToken(String token) {
         return token
-            .replaceAll("(","-LRB-")
-            .replaceAll(")","-RRB-")
-            .replaceAll("[","-LSB-")
-            .replaceAll("]","-RSB-")
-            .replaceAll("{","-LCB-")
-            .replaceAll("}","-RCB-")
+            .replaceAll("\\(","-LRB-")
+            .replaceAll("\\)","-RRB-")
+            .replaceAll("\\[","-LSB-")
+            .replaceAll("\\]","-RSB-")
+            .replaceAll("\\{","-LCB-")
+            .replaceAll("\\}","-RCB-")
             .replaceAll("\u201C","``")
             .replaceAll("\u201D","''");
     }
@@ -40,8 +45,28 @@ public class PennTreebankTokenizerFactory
     public static final TokenizerFactory INSTANCE
         = new PennTreebankTokenizerFactory();
 
-
-    static final String BASE_REGEX = "(\.\.\.";
+    static final String BASE_REGEX
+        = "("
+        + "\\.\\.\\."
+        + "|" + "--"
+        + "|" + "'(?<![\\p{Z}])"
+        + "|" + "'(ll|re|ve|s|d|M)" // adding 'm' (only lower) breaks disjunction.  wtf?
+        + "|" + "ca(?=(n't|N'T))"
+        + "|" + "can(?=not)"
+        + "|" + "n't|N'T"
+        + "|" + "[\\p{L}\\p{N}]+$"
+        + "|" + "[\\p{L}\\p{N}\\.]+"
+        + "|" + "[^\\p{Z}]"
+        + ")";
     
+    public static void main(String[] args) {
+        String text = args[0];
+        DisplayTokens.displayTextPositions(text);
+        TokenizerFactory tf = PennTreebankTokenizerFactory.INSTANCE;
+        DisplayTokens.displayTokens(text,tf);
+    }
+
+
+
 
 }
