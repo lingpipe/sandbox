8fcb964e64cb014be608aa569ffb5010e13c2fa3
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-07-31 00:16:44 +0000

    more chars

--- lpbook/src/tok/src/com/lingpipe/book/tok/PennTreebankTokenizerFactory.java
+++ lpbook/src/tok/src/com/lingpipe/book/tok/PennTreebankTokenizerFactory.java
@@ -3,19 +3,45 @@ package com.lingpipe.book.tok;
 public class PennTreebankTokenizerFactory
     extends ModifyTokenTokenizerFactory {
 
-    public static TokenizerFactory INSTANCE
-        = new PennTreebankTokenizerFactory();
-
     private PennTreebankTokenizerFactory() {
         super(new RegExTokenizerFactory(BASE_REGEX));
     }
 
-    public String modifyToken(String token) {
-        String modToken = REPLACE_MAP.get(token);
-        return modToken == null
-            ? token
-            : modToken;
+    @Override
+    public Tokenizer tokenizer(char[] cs, int start, int len) {
+
+        // replace quotes at begin-of-string and after start
+        // punctuation (Zs,Ps) with open-curly quotes, others with
+        // close curlies, then replace directional punctuation
+        // (Pi,Pf) with open and close.
+        // does not change length so spans are correct
+        String s = new String(cs,start,len)
+            .replaceAll("(^\")","\u201C")
+            .replaceAll("(?:[\\p{Zs}\\p{Ps}])\"","\u201C")
+            .replaceAll("\"","\u201D")
+            .replaceAll("\\{Pi}","\u201C")
+            .replaceAll("\\{Pf}","\u201D");
+        return super.tokenizer(s.toCharArray(),0,len);
     }
 
+    @Override
+    public String modifyToken(String token) {
+        return token
+            .replaceAll("(","-LRB-")
+            .replaceAll(")","-RRB-")
+            .replaceAll("[","-LSB-")
+            .replaceAll("]","-RSB-")
+            .replaceAll("{","-LCB-")
+            .replaceAll("}","-RCB-")
+            .replaceAll("\u201C","``")
+            .replaceAll("\u201D","''");
+    }
+
+    public static final TokenizerFactory INSTANCE
+        = new PennTreebankTokenizerFactory();
+
+
+    static final String BASE_REGEX = "(\.\.\.";
+    
 
 }
