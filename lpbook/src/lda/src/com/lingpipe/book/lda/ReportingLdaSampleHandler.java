f475a546ec9eb22d6eac68c4f786db9fed288ac6
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-08-20 19:37:40 +0000

    finished synthetic lda walk through

--- lpbook/src/lda/src/com/lingpipe/book/lda/ReportingLdaSampleHandler.java
+++ lpbook/src/lda/src/com/lingpipe/book/lda/ReportingLdaSampleHandler.java
@@ -28,54 +28,61 @@ public class ReportingLdaSampleHandler
     }
 /*x*/
 
+    /*x ReportingLdaSampleHandler.2 */
     public void handle(GibbsSample sample) {
-        if ((sample.epoch() % mReportPeriod) != 0) return;
+        int epoch = sample.epoch();
+        if ((epoch % mReportPeriod) != 0) return;
         long t = System.currentTimeMillis() - mStartTime;
-        double xEntropyRate = -sample.corpusLog2Probability()
-            / sample.numTokens();
+        double xEntropyRate 
+            = -sample.corpusLog2Probability() / sample.numTokens();
+    /*x*/
         System.out.printf("n=%6d t=%8s x-entropy-rate=%8.4f\n",
-                          sample.epoch(), Strings.msToString(t),
+                          epoch, Strings.msToString(t),
                           xEntropyRate);
     }
 
-    void fullReport(GibbsSample sample,
-                    int maxWordsPerTopic,
-                    int maxTopicsPerDoc,
-                    boolean reportTokens) {
-
-        System.out.println("\nFull Report");
-
-        int numTopics = sample.numTopics();
-        int numWords = sample.numWords();
-        int numDocs = sample.numDocuments();
-        int numTokens = sample.numTokens();
-
+    public void reportParameters(GibbsSample sample) {
         System.out.println("sample=" + sample.epoch());
-        System.out.println("numDocs=" + numDocs);
-        System.out.println("numTokens=" + numTokens);
-        System.out.println("numWords=" + numWords);
-        System.out.println("numTopics=" + numTopics);
+        System.out.println("numDocuments=" + sample.numDocuments());
+        System.out.println("numTokens=" + sample.numTokens());
+        System.out.println("numWords=" + sample.numWords());
+        System.out.println("numTopics=" + sample.numTopics());
+    }
 
-        for (int topic = 0; topic < numTopics; ++topic) {
+    /*x ReportingLdaSampleHandler.3 */
+    public void reportTopics(GibbsSample sample, int maxWords) {
+        for (int topic = 0; topic < sample.numTopics(); ++topic) {
             int topicCount = sample.topicCount(topic);
-            ObjectToCounterMap<Integer> counter = new ObjectToCounterMap<Integer>();
-            for (int word = 0; word < numWords; ++word)
-                counter.set(Integer.valueOf(word),sample.topicWordCount(topic,word));
-            List<Integer> topWords = counter.keysOrderedByCountList();
+            ObjectToCounterMap<Integer> counter 
+                = new ObjectToCounterMap<Integer>();
+            for (int word = 0; word < sample.numWords(); ++word)
+                counter.set(word, 
+                            sample.topicWordCount(topic,word));
+            List<Integer> topWords 
+                = counter.keysOrderedByCountList();
+    /*x*/
+
             System.out.println("\nTOPIC " + topic  + "  (total count=" + topicCount + ")");
             System.out.println("SYMBOL             WORD    COUNT   PROB          Z");
             System.out.println("--------------------------------------------------");
-            for (int rank = 0; rank < maxWordsPerTopic && rank < topWords.size(); ++rank) {
+
+   /*x ReportingLdaSampleHandler.4 */
+            for (int rank = 0; 
+                 rank < maxWords && rank < topWords.size(); 
+                 ++rank) {
+
                 int wordId = topWords.get(rank);
                 String word = mSymbolTable.idToSymbol(wordId);
                 int wordCount = sample.wordCount(wordId);
-                int topicWordCount = sample.topicWordCount(topic,wordId);
-                double topicWordProb = sample.topicWordProb(topic,wordId);
+                int topicWordCount 
+                    = sample.topicWordCount(topic,wordId);
+                double topicWordProb 
+                    = sample.topicWordProb(topic,wordId);
                 double z = binomialZ(topicWordCount,
                                      topicCount,
                                      wordCount,
-                                     numTokens);
-
+                                     sample.numTokens());
+    /*x*/
                 System.out.printf("%6d  %15s  %7d   %4.3f  %8.1f\n",
                                   wordId,
                                   word,
@@ -84,36 +91,55 @@ public class ReportingLdaSampleHandler
                                   z);
             }
         }
+    }
 
-        for (int doc = 0; doc < numDocs; ++doc) {
-            int docCount = 0;
-            for (int topic = 0; topic < numTopics; ++topic)
-                docCount += sample.documentTopicCount(doc,topic);
-            ObjectToCounterMap<Integer> counter = new ObjectToCounterMap<Integer>();
-            for (int topic = 0; topic < numTopics; ++topic)
-                counter.set(Integer.valueOf(topic),sample.documentTopicCount(doc,topic));
-            List<Integer> topTopics = counter.keysOrderedByCountList();
+    /*x ReportingLdaSampleHandler.6 */
+    public void reportDocs(GibbsSample sample, int maxTopics, 
+                           boolean reportTokens) {
+        for (int doc = 0; doc < sample.numDocuments(); ++doc) {
+            ObjectToCounterMap<Integer> counter 
+                = new ObjectToCounterMap<Integer>();
+            for (int topic = 0; 
+                 topic < sample.numTopics(); 
+                 ++topic)
+                counter.set(topic,
+                            sample.documentTopicCount(doc,topic));
+            List<Integer> topTopics 
+                = counter.keysOrderedByCountList();
+    /*x*/          
+
+
             System.out.println("\nDOC " + doc);
             System.out.println("TOPIC    COUNT    PROB");
             System.out.println("----------------------");
-            for (int rank = 0; rank < topTopics.size() && rank < maxTopicsPerDoc; ++rank) {
+    /*x ReportingLdaSampleHandler.7 */
+            for (int rank = 0; 
+                 rank < topTopics.size() && rank < maxTopics; 
+                 ++rank) {
+
                 int topic = topTopics.get(rank);
-                int docTopicCount = sample.documentTopicCount(doc,topic);
-                double docTopicPrior = sample.documentTopicPrior();
-                double docTopicProb = (sample.documentTopicCount(doc,topic) + docTopicPrior)
-                    / (docCount + numTopics * docTopicPrior);
+                int docTopicCount 
+                    = sample.documentTopicCount(doc,topic);
+                double docTopicProb 
+                    = sample.documentTopicProb(doc,topic);
+    /*x*/
+
                 System.out.printf("%5d  %7d   %4.3f\n",
                                   topic,
                                   docTopicCount,
                                   docTopicProb);
             }
+
             System.out.println();
+
+    /*x ReportingLdaSampleHandler.8 */
             if (!reportTokens) continue;
             int numDocTokens = sample.documentLength(doc);
             for (int tok = 0; tok < numDocTokens; ++tok) {
                 int symbol = sample.word(doc,tok);
                 short topic = sample.topicSample(doc,tok);
                 String word = mSymbolTable.idToSymbol(symbol);
+    /*x*/
                 System.out.print(word + "(" + topic + ") ");
             }
             System.out.println();
@@ -121,16 +147,18 @@ public class ReportingLdaSampleHandler
     }
 
 
-    static double binomialZ(double wordCountInDoc, double wordsInDoc,
-                            double wordCountinCorpus, double wordsInCorpus) {
+    /*x ReportingLdaSampleHandler.5 */
+    static double binomialZ(double wordCountInDoc, 
+                            double wordsInDoc,
+                            double wordCountinCorpus, 
+                            double wordsInCorpus) {
         double pCorpus = wordCountinCorpus / wordsInCorpus;
         double var = wordsInCorpus * pCorpus * (1 - pCorpus);
         double dev = Math.sqrt(var);
         double expected = wordsInDoc * pCorpus;
-        double z = (wordCountInDoc - expected) / dev;
-        return z;
+        return (wordCountInDoc - expected) / dev;
     }
-
+    /*x*/
 
 
 }
