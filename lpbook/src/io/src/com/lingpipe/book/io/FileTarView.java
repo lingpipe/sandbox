563d6ea55c127e1ecd57b31ae11d9590f5b6677d
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-09-04 21:56:16 +0000

    new io section and started corpus chapter

---
+++ lpbook/src/io/src/com/lingpipe/book/io/FileTarView.java
@@ -0,0 +1,61 @@
+package com.lingpipe.book.io;
+
+import com.aliasi.util.Streams;
+
+import org.apache.tools.tar.TarEntry;
+import org.apache.tools.tar.TarInputStream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+import java.util.Date;
+
+import java.util.zip.GZIPInputStream;
+
+public class FileTarView {
+
+    public static void main(String[] args) throws IOException {
+        File tarFile = new File(args[0]);
+        boolean gzipped = Boolean.parseBoolean(args[1]);
+
+        /*x FileTarView.1 */
+        InputStream fileIn = new FileInputStream(tarFile);
+        InputStream in = gzipped
+            ? new GZIPInputStream(fileIn)
+            : fileIn;
+        TarInputStream tarIn = new TarInputStream(in);
+        /*x*/
+
+        /*x FileTarView.2 */
+        while (true) {
+            TarEntry entry = tarIn.getNextEntry();
+            if (entry == null) break;
+
+            boolean isDirectory = entry.isDirectory();
+            String name = entry.getName();
+            String userName = entry.getUserName();
+            String groupName = entry.getGroupName();
+            int mode = entry.getMode();
+            Date date = entry.getModTime();
+            long size = entry.getSize();
+
+            byte[] bs = Streams.toByteArray(tarIn);
+        /*x*/
+            System.out.println("------------------------------------");
+            System.out.println("name=" + name);
+            System.out.println("isDirectory=" + isDirectory);
+            System.out.println("userName=" + userName);
+            System.out.println("groupName=" + groupName);
+            System.out.println("mode=" + Integer.toOctalString(mode));
+            System.out.println("date=" + date);
+            System.out.println("size=" + size);
+            System.out.println("#bytes read=" + bs.length);
+        }
+        /*x FileTarView.3 */        
+        tarIn.close();
+        /*x*/
+    }
+
+} 
