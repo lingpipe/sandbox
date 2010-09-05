2a96ba884c38e6ff3d8847a42d9ce05d30d1a5e2
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-09-05 01:24:28 +0000

    updated new text stuff

--- lpbook/src/corpus/src/com/lingpipe/book/corpus/TextCorpusAdapter.java
+++ lpbook/src/corpus/src/com/lingpipe/book/corpus/TextCorpusAdapter.java
@@ -5,26 +5,30 @@ import com.aliasi.classify.Classified;
 import com.aliasi.corpus.Corpus;
 import com.aliasi.corpus.ObjectHandler;
 
+import java.io.IOException;
+
 public class TextCorpusAdapter
     extends Corpus<ObjectHandler<CharSequence>> {
     
-    final Corpus<Classified<CharSequence>> mCp;
+    final Corpus<ObjectHandler<Classified<CharSequence>>> mCorpus;
 
-    public TextCorpusAdapter(Corpus<Classified<CharSequence>> cp) {
-                             corpus) {
+    public TextCorpusAdapter(Corpus<ObjectHandler<Classified<CharSequence>>> corpus) {
         mCorpus=corpus;
     }
 
     @Override
-    public void visitTrain(ObjectHandler<CharSequence> handler) {
-        mCp.visitTrain(new Forgetter(handler));
+    public void visitTrain(ObjectHandler<CharSequence> handler) 
+        throws IOException {
+
+        mCorpus.visitTrain(new Forgetter(handler));
     }
 
     @Override
     public void visitTest(final ObjectHandler<CharSequence> 
-                          handler) {
+                          handler) 
+        throws IOException {
         
-        mCp.visitTest(new ObjectHandler<Classified<CharSequence>>() {
+        mCorpus.visitTest(new ObjectHandler<Classified<CharSequence>>() {
                 public void handle(Classified<CharSequence> c) {
                     handler.handle(c.getObject());
                 }
@@ -35,7 +39,7 @@ public class TextCorpusAdapter
     static class Forgetter 
         implements ObjectHandler<Classified<CharSequence>> {
 
-        final ObjectHandler<CharSequence> mTxtHandler;
+        final ObjectHandler<CharSequence> mTextHandler;
 
         public Forgetter(ObjectHandler<CharSequence> textHandler) {
             mTextHandler = textHandler;
