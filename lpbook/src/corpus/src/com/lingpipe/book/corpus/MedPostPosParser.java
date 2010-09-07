47df7325e1db0b74f2b7db1658c1b475e4aa7e01
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-09-07 00:13:16 +0000

    lots of writing and coding for corpus

--- lpbook/src/corpus/src/com/lingpipe/book/corpus/MedPostPosParser.java
+++ lpbook/src/corpus/src/com/lingpipe/book/corpus/MedPostPosParser.java
@@ -1,29 +1,88 @@
 package com.lingpipe.book.corpus;
 
+import com.aliasi.corpus.ObjectHandler;
+import com.aliasi.corpus.Parser;
+import com.aliasi.corpus.StringParser;
+
+import com.aliasi.io.FileExtensionFilter;
+
+import com.aliasi.tag.Tagging;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/*x MedPostPosParser.1 */
 public class MedPostPosParser
     extends StringParser<ObjectHandler<Tagging<String>>> {
+/*x*/
     
+    /*x MedPostPosParser.2 */
+    @Override
     public void parseString(char[] cs, int start, int end) {
         String in = new String(cs,start,end-start);
-        for (String sentence : in.split("\n")) {
+        for (String sentence : in.split("\n"))
             if (sentence.indexOf('_') >= 0) 
                 process(sentence);
-        }
     }
+    /*x*/
+    
 
+    /*x MedPostPosParser.3 */
     private void process(String sentence) {
-        String[] tagTokenPairs = sentence.split(" ");
         List<String> tokenList = new ArrayList<String>();
         List<String> tagList = new ArrayList<String>();
         
-        for (String pair : tagTokenPairs) {
+        for (String pair : sentence.split(" ")) {
             String[] tokTag = pair.split("_");
-            tokenList.add(tokTag[0].trim());
-            tagList.add(tokTag[1].trim());
+            tokenList.add(tokTag[0]);
+            tagList.add(tokTag[1]);
         }
         Tagging<String> tagging
             = new Tagging<String>(tokenList,tagList);
-        getHandler().handle(tagging);
+        ObjectHandler<Tagging<String>> handler = getHandler();
+        handler.handle(tagging);
+    }
+    /*x*/
+
+    public static void main(String[] args) throws IOException {
+        File medPostDir = new File(args[0]);
+        System.out.println("medPostDir=" + medPostDir.getCanonicalPath());
+
+        /*x MedPostPosParser.4 */
+        final Set<String> tagSet = new TreeSet<String>();
+        ObjectHandler<Tagging<String>> handler 
+
+            = new ObjectHandler<Tagging<String>>() {
+
+                public void handle(Tagging<String> tagging) {
+                    tagSet.addAll(tagging.tags());
+                }
+
+        };
+        /*x*/
+
+        /*x MedPostPosParser.5 */
+        Parser<ObjectHandler<Tagging<String>>> parser
+            = new MedPostPosParser();
+        parser.setHandler(handler);
+        /*x*/
+
+        /* MedPostPosParser.6 */
+        FileFilter iocFilter = new FileExtensionFilter("ioc");
+        for (File file : medPostDir.listFiles(iocFilter))
+            parser.parse(file,"ASCII");
+        /*x*/
+
+        System.out.println("#Tags=" + tagSet.size());
+        System.out.println("Tags=");
+        for (String tag : tagSet)
+            System.out.println("     " + tag);
     }
 
 }
