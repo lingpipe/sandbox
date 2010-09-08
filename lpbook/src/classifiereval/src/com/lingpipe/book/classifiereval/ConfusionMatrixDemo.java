f2d142a12c4e5ed56ad55949e372ee6fdfef24af
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-09-08 18:01:01 +0000

    added classifier eval chapter

---
+++ lpbook/src/classifiereval/src/com/lingpipe/book/classifiereval/ConfusionMatrixDemo.java
@@ -0,0 +1,25 @@
+package com.lingpipe.book.classifiereval;
+
+import com.aliasi.classify.ConfusionMatrix;
+
+public class ConfusionMatrixDemo {
+
+    private ConfusionMatrixDemo() { /* no instances */ }
+
+    public static void main(String[] args) {
+        /*x ConfusionMatrixDemo.1 */
+        String[] cats = new String[] {
+            "cabernet", "syrah", "pinot"
+        };
+        int[][] cells = new int[][] {
+            { 9, 3, 0 },
+            { 3, 5, 1 },
+            { 1, 1, 4 }
+        };
+        ConfusionMatrix cm
+            = new ConfusionMatrix(cats, cells);
+        /*x*/
+
+    }
+
+}
