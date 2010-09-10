cc66b306cfd6ac1a0a7766c974b1210a0999fbd5
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-09-10 23:41:56 +0000

    more classifier evals

--- lpbook/src/classifiereval/src/com/lingpipe/book/classifiereval/ConfusionMatrixDemo.java
+++ lpbook/src/classifiereval/src/com/lingpipe/book/classifiereval/ConfusionMatrixDemo.java
@@ -22,6 +22,84 @@ public class ConfusionMatrixDemo {
         ConfusionMatrix cm = new ConfusionMatrix(cats, cells);
         /*x*/
 
+        /*x ConfusionMatrixDemo.2 */
+        String[] categories = cm.categories();
+
+        int totalCount = cm.totalCount();
+        int totalCorrect= cm.totalCorrect();
+        double totalAccuracy = cm.totalAccuracy();
+        double confidence95 = cm.confidence95();
+        double confidence99 = cm.confidence99();
+        /*x*/
+
+        /*x ConfusionMatrixDemo.3 */
+        double randomAccuracy = cm.randomAccuracy();
+        double randomAccuracyUnbiased = cm.randomAccuracyUnbiased();
+        double kappa = cm.kappa();
+        double kappaUnbiased = cm.kappaUnbiased();
+        double kappaNoPrevalence = cm.kappaNoPrevalence();
+        /*x*/
+
+        /*x ConfusionMatrixDemo.4 */
+        double referenceEntropy = cm.referenceEntropy();
+        double responseEntropy = cm.responseEntropy();
+        double crossEntropy = cm.crossEntropy();
+        double jointEntropy = cm.jointEntropy();
+        double conditionalEntropy = cm.conditionalEntropy();
+        double mutualInformation = cm.mutualInformation();
+        double klDivergence = cm.klDivergence();
+
+        double conditionalEntropyCab = cm.conditionalEntropy(0);
+        double conditionalEntropySyr = cm.conditionalEntropy(1);
+        double conditionalEntropyPin = cm.conditionalEntropy(2);
+        /*x*/
+
+        /*x ConfusionMatrixDemo.5 */
+        double chiSquared = cm.chiSquared();
+        double chiSquaredDegreesOfFreedom
+            = cm.chiSquaredDegreesOfFreedom();
+        double phiSquared = cm.phiSquared();
+        double cramersV = cm.cramersV();
+        
+        double lambdaA = cm.lambdaA();
+        double lambdaB = cm.lambdaB();
+        /*x*/
+        
+        
+        System.out.println("categories[0]=" + categories[0]);
+        System.out.println("categories[1]=" + categories[1]);
+        System.out.println("categories[2]=" + categories[2]);
+        System.out.println("totalCount=" + totalCount);
+        System.out.println("totalCorrect=" + totalCorrect);
+        System.out.println("totalAccuracy=" + totalAccuracy);
+        System.out.println("confidence95=" + confidence95);
+        System.out.println("confidence99=" + confidence99);
+
+        System.out.println("randomAccuracy=" + randomAccuracy);
+        System.out.println("randomAccuracyUnbiased=" + randomAccuracyUnbiased);
+        System.out.println("kappa=" + kappa);
+        System.out.println("kappaUnbiased=" + kappaUnbiased);
+        System.out.println("kappaNoPrevalence=" + kappaNoPrevalence);
+
+        System.out.println("referenceEntropy=" + referenceEntropy);
+        System.out.println("responseEntropy=" + responseEntropy);
+        System.out.println("crossEntropy=" + crossEntropy);
+        System.out.println("jointEntropy=" + jointEntropy);
+        System.out.println("conditionalEntropy=" + conditionalEntropy);
+        System.out.println("mutualInformation=" + mutualInformation);
+        System.out.println("klDivergence=" + klDivergence);
+        System.out.println("conditionalEntropyCab=" + conditionalEntropyCab);
+        System.out.println("conditionalEntropySyr=" + conditionalEntropySyr);
+        System.out.println("conditionalEntropyPin=" + conditionalEntropyPin);
+
+        System.out.println("chiSquared=" + chiSquared);
+        System.out.println("chiSquaredDegreesOfFreedom=" + chiSquaredDegreesOfFreedom);
+        System.out.println("phiSquared=" + phiSquared);
+        System.out.println("cramersV=" + cramersV);
+
+        System.out.println("lambdaA=" + lambdaA);
+        System.out.println("lambdaB=" + lambdaB);
+        
     }
 
 }
