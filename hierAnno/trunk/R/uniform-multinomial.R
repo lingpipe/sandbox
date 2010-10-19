28f9df4e078f13673ed76d0ac3315bb2054fa29c
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-10-19 16:13:35 +0000

    functions for multi

--- hierAnno/trunk/R/uniform-multinomial.R
+++ hierAnno/trunk/R/uniform-multinomial.R
@@ -1,22 +1,26 @@
 library("R2WinBUGS")
 
+mybugs <- function(data,inits,parameters,iter=1000,chains=3) {
+    bugs(data, inits, parameters,
+         "c:/carp/sandbox/hierAnno/R/bugs/beta-binomial-item.bug",
+          n.chains=chains, 
+          n.iter=iter,
+          debug=TRUE, 
+          clearWD=TRUE,
+          bugs.directory="c:\\WinBUGS\\WinBUGS14"
+          DIC=FALSE)
+}
+
 
 anno <- function(alpha,beta,y,jj,ii) {
     K <- max(y);
     J <- max(jj);
     I <- max(ii);
     alpha <- rep(0,K);
-    
-
     data <- list("alpha","beta","y","jj","ii","J","K","N")
     parameters <- c("pi","z","theta")
     inits <- function() {
         list(pi=runif(1,0.4,6),
              alpha
-    anno <- bugs(data, inits, parameters,
-                 "c:/carp/sandbox/hierAnno/R/bugs/beta-binomial-item.bug",
-                 n.chains=3, n.iter=100,
-                 debug=TRUE, clearWD=TRUE,
-                 bugs.directory="c:\\WinBUGS\\WinBUGS14")
-
+    mybugs(data,init,parameters);
 }
