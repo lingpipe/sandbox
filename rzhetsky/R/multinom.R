6d32e410417e1f19fb803cf3ea9d1705b26c63e6
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-11-01 22:52:10 +0000

    some rearranging of models

--- rzhetsky/R/multinom.R
+++ rzhetsky/R/multinom.R
@@ -1,1 +1,18 @@
+var pi[K], z[I], theta[J,K], alpha[K], phi[K], tau, y[N], unitK[K], preferK[K,K];
+
+data {
+    for (k in 1:K) {
+        unitK[k] = 1
+    }
+    for (k in 1:K) {
+        for (k2 in 1:(k-1)) {
+           prefer[k,k2] = 1
+        }
+        for (k2 in (k+1):K) {
+            prefer[k,k2] = 1
+        }
+        prefer[k,k] = K
+    }
+}
+
 
