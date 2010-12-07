fbcd9cf8053259199f339ed21f0c7893fdc630f1
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-12-07 23:18:19 +0000

    more nb eg

---
+++ lpbook/src/naivebayes/src/com/lingpipe/book/naivebayes/FragmentsNb.java
@@ -0,0 +1,35 @@
+package com.lingpipe.book.naivebayes;
+
+import java.util.Arrays;
+import java.util.List;
+
+class FragmentsNb {
+
+    void a1() {
+        /*x FragmentsNb.1 */
+        List<String> hisTexts 
+            = Arrays.asList("haw", "haw hee haw", "haw haw");
+        /*x*/
+    }
+
+    void a2() {
+        /*x FragmentsNb.2 */
+            Arrays.asList("hee", "haw haw haw haw", "haw");
+        /*x*/
+    }
+
+    void a3() {
+        /*x FragmentsNb.3 */
+            Arrays.asList("haw haw haw haw haw hee", "", "");
+        /*x*/
+    }
+
+
+    void a4() {
+        /*x FragmentsNb.4 */
+            Arrays.asList("haw haw haw haw haw hee");
+        /*x*/
+    }
+        
+
+}
