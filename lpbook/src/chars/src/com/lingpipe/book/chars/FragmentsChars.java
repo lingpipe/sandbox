b40b5849a07a267fd807f2b1f92ac4fd820ae1dd
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-12-07 21:36:53 +0000

    removed all verbatim-included code and replaced with fragments that compile

--- lpbook/src/chars/src/com/lingpipe/book/chars/FragmentsChars.java
+++ lpbook/src/chars/src/com/lingpipe/book/chars/FragmentsChars.java
@@ -44,5 +44,134 @@ class FragmentsChars {
         /*x*/
     }
 
+    void abc1() {
+        int codepoint = 0;
+        /*x FragmentsChars.6 */
+        int LEAD_OFFSET = 0xD800 - (0x10000 >> 10);
+        int SURROGATE_OFFSET = 0x10000 - (0xD800 << 10) - 0xDC00;
+
+        int lead = LEAD_OFFSET + (codepoint >> 10);
+        int trail = 0xDC00 + (codepoint & 0x3FF);
+        
+        int byte1 = lead >>> 8;
+        int byte2 = lead & 0xFF;
+        int byte3 = trail >>> 8;
+        int byte4 = trail & 0xFF;
+        /*x*/
+    }
+
+    void abc2() {
+        int byte1 = 0;
+        int byte2 = 0;
+        int byte3 = 0;
+        int byte4 = 0;
+        int SURROGATE_OFFSET = 0x10000 - (0xD800 << 10) - 0xDC00;
+
+        /*x FragmentsChars.7 */
+        int lead = byte1 << 8 | byte2;
+        int trail = byte3 << 8 | byte4;
+
+        int codepoint = (lead << 10) + trail + SURROGATE_OFFSET;
+        /*x*/
+    }
+
+    void abc3() {
+        /*x FragmentsChars.8 */
+        int n = 1;
+        /*x*/
+    }
+
+    void abc3b() {
+        /*x FragmentsChars.9 */
+        \u0069\u006E\u0074\u0020\u006E\u0020\u003D\u0020\u0031\u003B
+        /*x*/
+    }
+
+    void abc4() {
+        /*x FragmentsChars.10 */
+        char c = 'a'; 
+        /*x*/
+    }
+
+    void abc4b() {
+        /*x FragmentsChars.11 */
+        char c = '\u00E0'; 
+        /*x*/
+    }
+
+    void abc4c() {
+        /*x FragmentsChars.12 */
+        char c = 0x00E0;
+        /*x*/
+    }
+
+    void abc5() {
+        /*x FragmentsChars.13 */
+        int n = 'a';
+        /*x*/
+    }
+
+    void abc6() {
+        CharSequence cs = null;
+        /*x FragmentsChars.14 */
+        for (int i = 0; i < cs.length(); ++i) {
+            char c = cs.charAt(i);
+            // do something
+        /*x*/
+        }
+    }
+
+
+    void abc7() {
+        /*x FragmentsChars.15 */
+        String name = "Fred";
+        /*x*/
+    }
+
+
+    void abc8() {
+        /*x FragmentsChars.16 */
+        String name = "\u0046r\u0065d";
+        /*x*/
+    }
+
+
+    int abc9() {
+        String s = null;
+        String t = null;
+        /*x FragmentsChars.17 */
+        for (int i = 0; i < Math.min(s.length(),t.length()); ++i)
+            if (s.charAt(i) != t.charAt(i))
+                return s.charAt(i) - t.charAt(i);
+        return s.length() - t.length();
+        /*x*/
+    }
+
+
+    void abc10() {
+        String s = null;
+        /*x FragmentsChars.18 */
+        int hashCode = 0;
+        for (int i = 0; i < s.length(); ++i)
+            hashCode = 31 * hashCode + s.charAt(i);
+        /*x*/
+    }
+
+    void abc11() {
+        String[] xs = null;
+        /*x FragmentsChars.19 */
+        StringBuilder sb = new StringBuilder();
+        for (String x : xs)
+            sb.append(x);
+        String s = sb.toString();
+        /*x*/
+    }
+
+
+    void abc12() {
+        /*x FragmentsChars.20 */
+        /*x*/
+    }
+
 
 }
--- lpbook/src/tok/src/com/lingpipe/book/tok/FragmentsTok.java
+++ lpbook/src/tok/src/com/lingpipe/book/tok/FragmentsTok.java
@@ -1,15 +1,25 @@
 package com.lingpipe.book.tok;
 
 import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
+import com.aliasi.tokenizer.LowerCaseTokenizerFactory;
+import com.aliasi.tokenizer.RegExFilteredTokenizerFactory;
+import com.aliasi.tokenizer.StopTokenizerFactory;
 import com.aliasi.tokenizer.Tokenizer;
 import com.aliasi.tokenizer.TokenizerFactory;
 
+import com.aliasi.util.CollectionUtils;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+import java.util.regex.Pattern;
 
 class FragmentsTok {
 
-    public static void frag1() { 
+    void frag1() { 
         char[] cs = new char[0];
         TokenizerFactory tokFactory 
             = IndoEuropeanTokenizerFactory.INSTANCE;
@@ -20,7 +30,7 @@ class FragmentsTok {
         /*x*/
     }
 
-    public static void frag2() {
+    void frag2() {
         char[] cs = new char[0];
         TokenizerFactory tokFactory 
             = IndoEuropeanTokenizerFactory.INSTANCE;
@@ -33,5 +43,73 @@ class FragmentsTok {
         /*x*/
     }
     
+
+    void frag3() {
+        TokenizerFactory tokFac = null;
+        char[] cs = null;
+        int start = 0;
+        int length = 0;
+        /*x FragmentsTok.3 */
+        Tokenizer tokenizer = tokFac.tokenizer(cs,start,length);
+        for (String token : tokenizer) { 
+            // do something
+        }
+        /*x*/
+    }
+
+    TokenizerFactory baseTokenizerFactory() {
+        return null;
+    }
+
+    void frag4() {
+        /*x FragmentsTok.5 */
+        TokenizerFactory f = IndoEuropeanTokenizerFactory.INSTANCE;
+        Locale loc = Locale.GERMAN;
+        TokenizerFactory fact = new LowerCaseTokenizerFactory(f,loc);
+        /*x*/
+    }
+
+    void frag5() {
+        /*x FragmentsTok.6 */
+        Set<String> stopSet = CollectionUtils.asSet("the","be");
+        TokenizerFactory f1 = IndoEuropeanTokenizerFactory.INSTANCE;
+        TokenizerFactory f2 = new LowerCaseTokenizerFactory(f1);
+        TokenizerFactory f3 = new StopTokenizerFactory(f2,stopSet);
+        /*x*/
+    }
+
+    /*x FragmentsTok.7 */
+    static <E> HashSet<E> asSet(E... es) {
+    /*x*/
+        return null;
+    }
+
+
+    void frag6() {
+        TokenizerFactory f = null;
+        /*x FragmentsTok.8 */
+        Pattern p = Pattern.compile("\\p{Lu}.*");
+        TokenizerFactory f2 = new RegExFilteredTokenizerFactory(f,p);
+        /*x*/
+    }
+
+    void frag7() {
+        /*x FragmentsTok.9 */
+        /*x*/
+    }
+
+    abstract class Bar {
+        /*x FragmentsTok.4 */
+        protected abstract Tokenizer modify(Tokenizer tokenizer);
+
+        public Tokenizer tokenizer(char[] cs, int start, int length) {
+            TokenizerFactory tokFact = baseTokenizerFactory();
+            Tokenizer tok = tokFact.tokenizer(cs,start,length);
+            return modify(tok);
+        }
+        /*x*/
+    }
+
+
 
 }
