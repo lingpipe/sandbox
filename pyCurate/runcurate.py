ff9f326a4eca8a96d5095d4859029cb435e344b2
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-12-23 21:45:06 +0000

    simulation working

--- pyCurate/runcurate.py
+++ pyCurate/runcurate.py
@@ -1,34 +1,60 @@
-from libcurate import hello
+import libcurate
 import numpy
 import pymc
 
+libcurate.hello()
+libcurate.hello("bob")
 
-hello()
-hello("bob")
+I = 30
+J = 5
+K = 3   
+N = I*J 
 
-I = 200
-J = 3
-K = 3
-N = 600
+Is = range(I)
+Js = range(J)
+Ks = range(K)
+Ns = range(N)
 
-Is = range(I-1)
-Js = range(J-1)
-Ks = range(K-1)
-Ns = range(N-1)
+beta = [ 1, 1, 1 ]
+prevalence = pymc.rdirichlet(beta).tolist()
+prevalence.append(1.0-sum(prevalence)) # complete
+category = []
+for i in Is:
+    category.append(pymc.rcategorical(prevalence).tolist())
 
-prevalence = [ 0.3, 0.1, 0.6 ]
+alpha = [ [ 16, 4, 1 ],
+          [ 2, 16, 2 ],
+          [ 1, 4, 16 ] ]
+accuracy = []
+for j in Js:
+    accuracy.append([])
+    for k in Ks:
+        accuracy[j].append(pymc.rdirichlet(alpha[k]).tolist())
+        accuracy[j][k].append(1.0-sum(accuracy[j][k]))
 
 item = []
 anno = []
 label = []
+k = 0
+for i in Is:
+    for j in Js:
+        item.append(i)
+        anno.append(j)
+        label.append(pymc.rcategorical(accuracy[j][category[i]]).tolist())
+        k += 1
 
-category = [ [] ]
-for n in Ns
-accuracy 
+print "prevalence=",prevalence
+for j in Js:
+    for k in Ks:
+        print "acc ",j," ",k,"=",accuracy[j][k]
+k = 0
+for i in Is:
+    print "cat ",i,"=",category[i]
+    for j in Js:
+        print "     label ",j,"=",label[k]
+        k += 1
 
-
-alpha = [ [ 3, 1, 1 ],
-          [ 1, 3, 1 ],
-          [ 1, 1, 3 ] ]
-beta = [ 1, 1, 1 ]
-
+#for (prev,cat,acc) in em_dawid_skene(alpha,beta,item,anno,label):
+#    print "prev=", prev
+#    print "cat=", cat
+#    print "acc=", acc
