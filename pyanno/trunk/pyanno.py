4f1d898fa0117d71900b1339e7bf652671d9d36a
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-12-27 20:11:23 +0000

    major refactor

--- pyanno/trunk/pyanno.py
+++ pyanno/trunk/pyanno.py
@@ -1,21 +1,10 @@
 # Library Module
 from math import log
 
-def em_dawid_skene(item,   #int[N]
-                   anno,   #int[N]
-                   label): #int[N]
-    K = max(label)+1
-    alpha = fill_vec(K)
-    beta = fill_mat(K,K)
-    for x in em_ds_prior(item,anno,label,alpha,beta):
-        yield x
+def dawid_skene_mle_em(item,    # int[N]
+                       anno,    # int[N]
+                       label):  # int[N]
 
-
-def em_ds_prior(item,    # int[N]
-                anno,    # int[N]
-                label,   # int[N]
-                alpha,   # float[K][K]
-                beta):   # float[K]
     I = max(item)+1
     J = max(anno)+1
     K = max(label)+1
@@ -25,6 +14,87 @@ def em_ds_prior(item,    # int[N]
     Js = range(J)
     Ks = range(K)
     Ns = range(N)
+
+    warn_missing_vals("item",item)
+    warn_missing_vals("anno",anno)
+    warn_missing_vals("label",label)
+
+    # initialize params
+    init_accuracy = 0.6
+    prevalence = alloc_vec(K,1.0/K)
+    category = alloc_mat(I,K,1.0/K)
+    accuracy = alloc_tens(J,K,K,(1.0 - init_accuracy)/(K-1.0))
+    for j in Js:
+        for k in Ks:
+            accuracy[j][k][k] = init_accuracy
+    
+    while True:
+
+        # E: p(cat[i]|...) 
+        for i in Is:
+            list_copy(prevalence,category[i],Ks)
+        for n in Ns:
+            for k in Ks:
+                category[item[n]][k] *= accuracy[anno[n]][k][label[n]]
+
+        # log likelihood here to reuse intermediate category calc
+        log_likelihood = 0.0
+        for i in Is:
+            likelihood_i = 0.0
+            for k in Ks:
+                likelihood_i += category[i][k]
+            log_likelihood_i = log(likelihood_i)
+            log_likelihood += log_likelihood_i
+
+        for i in Is:
+            prob_norm(category[i],Ks)
+
+        # return here with E[cat|prev,acc] and LL(prev,acc;y)
+        yield (log_likelihood,prevalence,category,accuracy)
+
+        # M: prevalence* + accuracy*
+        fill_vec(prevalence,0.0)
+        for i in Is:
+            for k in Ks:
+                prevalence[k] += category[i][k]
+        prob_norm(prevalence,Ks)
+
+        fill_tens(accuracy,0.0)
+        for n in Ns:
+            for k in Ks:
+                accuracy[anno[n]][k][label[n]] += category[item[n]][k]
+        for j in Js:
+            for k in Ks:
+                prob_norm(accuracy[j][k],Ks)
+
+
+
+
+def em_ds_prior(item,            # int[N]
+                anno,            # int[N]
+                label,           # int[N]
+                alpha=None,      # float[K][K]
+                beta=None,       # float[K]
+                supervis=None):  # int[I]
+    I = max(item)+1
+    J = max(anno)+1
+    K = max(label)+1
+    N = len(item)
+
+    if supervis != None:
+        K = max(K,max(supervis))
+
+    Is = range(I)
+    Js = range(J)
+    Ks = range(K)
+    Ns = range(N)
+
+    if alpha == None:
+        alpha = alloc_vec(K)
+    if beta == None:
+        beta = alloc_mat(K,K)
+    if supervis == None:
+        supervis = alloc_vec(I,None)
 
     if len(alpha) != K:
         raise ValueError("len(alpha) != K")
@@ -37,10 +107,17 @@ def em_ds_prior(item,    # int[N]
         raise ValueError("len(anno) != N")
     if len(label) != N:
         raise ValueError("len(label) != N")
+    if len(supervis) != I:
+        raise ValueError("len(cat) != I")
 
-    prevalence = fill_vec(K,1.0/K)
-    category = fill_mat(I,K,1.0/K)
-    accuracy = fill_tens(J,K,K,0.3/(K-1.0))
+    prevalence = alloc_vec(K,1.0/K)
+    category = alloc_mat(I,K,1.0/K)
+    for i in Is:
+        if supervis[i] != None:
+            for k in Ks:
+                category[i][k] = 1.0 if k == supervis[k] else 0.0
+            category[i][supervis[i]] = 1.0
+    accuracy = alloc_tens(J,K,K,0.3/(K-1.0))
     for j in Js:
         for k in Ks:
             accuracy[j][k][k] = 0.7
@@ -51,7 +128,8 @@ def em_ds_prior(item,    # int[N]
             list_copy(prevalence,category[i],Ks)
         for n in Ns:
             for k in Ks:
-                category[item[n]][k] *= accuracy[anno[n]][k][label[n]]
+                if supervis[item[n]] == None:
+                    category[item[n]][k] *= accuracy[anno[n]][k][label[n]]
 
         log_likelihood = 0.0
         for i in Is:
@@ -86,18 +164,43 @@ def em_ds_prior(item,    # int[N]
                 prob_norm(accuracy[j][k],Ks)
 
 
+def warn_missing_vals(varname,xs):
+    missing = set(xs) - set(range(max(xs)+1))
+    if len(missing) > 0:
+        print "Missing values in ",varname,"=",missing
+
 def list_copy(froms,tos,indexes):
     for i in indexes:
         tos[i] = froms[i]
 
 
+def fill_vec(xs,y):
+    i = 0
+    while i < len(xs):
+        xs[i] = y
+        i += 1
+
+def fill_mat(xs,y):
+    i = 0
+    while i < len(xs):
+        fill_vec(xs[i],y)
+        i += 1
+
+def fill_tens(xs,y):
+    i = 0
+    while i < len(xs):
+        fill_mat(xs[i],y)
+        i += 1
+
 def prob_norm(theta,indexes):
     Z = sum(theta)
-    for i in indexes:
-        theta[i] /= Z
+    n = len(theta) - 1
+    while n >= 0:
+        theta[n] /= Z
+        n -= 1
 
 
-def fill_vec(N,x=0.0):
+def alloc_vec(N,x=0.0):
     result = []
     n = 0
     while n < N:
@@ -106,20 +209,20 @@ def fill_vec(N,x=0.0):
     return result
 
 
-def fill_mat(M,N,x=0.0):
+def alloc_mat(M,N,x=0.0):
     result = []
     m = 0
     while m < M:
-        result.append(fill_vec(N,x))
+        result.append(alloc_vec(N,x))
         m += 1
     return result
                    
     
-def fill_tens(M,N,J,x=0.0):
+def alloc_tens(M,N,J,x=0.0):
     result = []
     m = 0
     while m < M:
-        result.append(fill_mat(N,J,x))
+        result.append(alloc_mat(N,J,x))
         m += 1
     return result
             
--- pyanno/trunk/runpyanno.py
+++ pyanno/trunk/runpyanno.py
@@ -2,9 +2,9 @@ import pyanno
 import pymc
 import numpy
 
-I = 100
-J = 3
-K = 4
+I = 10000
+J = 5
+K = 5
 N = I*J 
 
 Is = range(I)
@@ -23,16 +23,14 @@ for i in Is:
 
 #doh!  alpha set up to do three-way only
 
-alpha = []
+alpha = pyanno.alloc_mat(K,K)
 for k1 in Ks:
-    alpha.append([])
     for k2 in Ks:
-        alpha[k1].append((5 - abs(k1 - k2))**2)
-accuracy = []
+        alpha[k1][k2] = (5 - abs(k1 - k2))**2
+accuracy = pyanno.alloc_tens(J,K,K)
 for j in Js:
-    accuracy.append([])
     for k in Ks:
-        accuracy[j].append(pymc.rdirichlet(alpha[k]).tolist())
+        accuracy[j][k] = pymc.rdirichlet(alpha[k]).tolist()
         accuracy[j][k].append(1.0-sum(accuracy[j][k]))
 
 item = []
@@ -46,6 +44,10 @@ for i in Is:
         label.append(pymc.rcategorical(accuracy[j][category[i]]).tolist())
         k += 1
 
+sample_prevalence = pyanno.alloc_vec(K)
+for k in label:
+    sample_prevalence[k] += 1
+pyanno.prob_norm(sample_prevalence,Ks)
 
 # k = 0
 # for i in Is:
@@ -58,15 +60,21 @@ for j in Js:
     for k in Ks:
         print "acc ",j," ",k,"=",accuracy[j][k]
 print "prevalence=",prevalence
+print "sample prevalence=",sample_prevalence
 
+delta = pyanno.alloc_vec(K)
 epoch = 0
-for (ll,prev,cat,acc) in pyanno.em_ds_prior(item,anno,label,alpha,beta):
+for (ll,prev,cat,acc) in pyanno.dawid_skene_mle_em(item,anno,label):
     if epoch > 1000: break
     print ""
     print "===================================="
     print "EPOCH=", epoch
     print "log likelihood=",ll
     print "prev=", prev
+    for k in Ks:
+        delta[k] = prev[k] - sample_prevalence[k]
+    print "estimated_prev - sample_prev=",delta
+    
     epoch += 1
 
 print "prev=", prev
