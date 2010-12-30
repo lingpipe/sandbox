cce0387bc8f8dfdc76cc7ba5847ebf922f7c6e8d
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2010-12-30 17:06:15 +0000

    refactored to new package struct

--- pyanno/trunk/README.txt
+++ pyanno/trunk/README.txt
@@ -1,10 +1,27 @@
-PyCurate 1.0 README
+README: pyanno 1.0
 ============================================================
 
-What is PyCurate?
-------------------------------------------------------------
-PyCurate 1.0 is a suite of Python libraries for curating
-data.
+What is pyanno?
+------------------------------------------------------------ 
+
+pyanno 1.0 is a suite of Python libraries for modeling the data coding
+or diagnostic testing process to support inferences required for data
+curation.  Specifically, pyanno implements statistical models for
+inferring from multiply coded categorical data 
+
+    * annotator accuracies and biases,
+    * gold standard categories of items,
+    * prevalence of categories in population, and
+    * population distribution of annotator accuracies and biases.
+
+The models include Dawid and Skene's (1979) multinomial model with a
+maximum likelihood estimator implemented with EM.  A generalization of
+Dawid and Skene's model with Dirichlet priors on prevalence and
+estimator accuracy is also implemented.  For this model, estimator
+implementations include a maximum a posteriori estimator via
+expectation maximization (EM), a Bayesian posterior sampler estimated
+via Gibbs sampling, and a Bayesian estimator implemented via averaging
+Gibbs samples from the posterior.
 
 
 Licensing
@@ -21,16 +38,17 @@ in Documentation.txt.
 
 Contributors
 ------------------------------------------------------------
-Bob Carpenter
-Andrey Rzhetsky
-James Evans
+Bob Carpenter    (Columbia University,   Statistics)
+Andrey Rzhetsky  (University of Chicago, Medicine)
+James Evans      (University of Chicago, Sociology)
 
 
 References
 ------------------------------------------------------------
+
 Dawid, A. P. and A. M. Skene. 1979. Maximum likelihood estimation of
-observed errorrates using the EM algorithm. Journal of Applied
-Statististics 28(1):20--28.
+observed error rates using the EM algorithm. Journal of Applied
+Statistics 28(1):20--28.
 
 Rzhetsky A., H. Shatkay, and W. J. Wilbur. 2009. How to get the most
 out of your curation effort. PLoS Computational Biology 5(5).
--- pyanno/trunk/runpyanno.py
+++ pyanno/trunk/runpyanno.py
@@ -1,7 +1,8 @@
-import pyanno
-import pymc
-import numpy
+import pyanno.multinom
 
+# if x is not none: ...
+
+# Simulated Sizes
 I = 10000
 J = 5
 K = 5
@@ -12,68 +13,63 @@ Js = range(J)
 Ks = range(K)
 Ns = range(N)
 
-beta = []
-for k in Ks:
-    beta.append(2)
-prevalence = pymc.rdirichlet(beta).tolist()
-prevalence.append(1.0-sum(prevalence)) # complete
-category = []
-for i in Is:
-    category.append(pymc.rcategorical(prevalence).tolist())
+# Simulate Full Data Set
+(prevalence,category,accuracy,item,anno,label) = pyanno.multinom.sim_dawid_skene(I,J,K)
 
-alpha = pyanno.alloc_mat(K,K)
-for k1 in Ks:
-    for k2 in Ks:
-        alpha[k1][k2] = (5 - abs(k1 - k2))**2
-accuracy = pyanno.alloc_tens(J,K,K)
+# Calculate Sample Params
+prevalence_sample = pyanno.multinom.alloc_vec(K)
+for k in category:
+    prevalence_sample[k] += 1
+pyanno.multinom.prob_norm(prevalence_sample,Ks)
+
+accuracy_sample = pyanno.multinom.alloc_tens(J,K,K)
+for n in Ns:
+    accuracy_sample[anno[n]][category[item[n]]][label[n]] += 1
 for j in Js:
     for k in Ks:
-        accuracy[j][k] = pymc.rdirichlet(alpha[k]).tolist()
-        accuracy[j][k].append(1.0-sum(accuracy[j][k]))
+        pyanno.multinom.prob_norm(accuracy_sample[j][k],Ks)
 
-item = []
-anno = []
-label = []
-k = 0
-for i in Is:
-    for j in Js:
-        item.append(i)
-        anno.append(j)
-        label.append(pymc.rcategorical(accuracy[j][category[i]]).tolist())
-        k += 1
-
-sample_prevalence = pyanno.alloc_vec(K)
-for k in label:
-    sample_prevalence[k] += 1
-pyanno.prob_norm(sample_prevalence,Ks)
-
-# k = 0
-# for i in Is:
-#     print "cat ",i,"=",category[i]
-#     for j in Js:
-#         print "     label ",j,"=",label[k]
-#         k += 1
-
-for j in Js:
-    for k in Ks:
-        print "acc ",j," ",k,"=",accuracy[j][k]
-print "prevalence=",prevalence
-print "sample prevalence=",sample_prevalence
-
-delta = pyanno.alloc_vec(K)
+# EM Fit
+print "RUNNING EM"
+MAX_EPOCHS = 500
+EPSILON = 0.001
 epoch = 0
-for (ll,prev,cat,acc) in pyanno.dawid_skene_mle_em(item,anno,label):
-    if epoch > 100: break
-    print "EPOCH={0:6d}  log likelihood={1:+10.4f}".format(epoch,ll)
+log_likelihood_curve = []
+diff = -1
+for (ll,prevalence_mle,category_mle,accuracy_mle) in pyanno.multinom.dawid_skene_mle_em(item,anno,label):
+    log_likelihood_curve.append(ll)
+    if epoch > MAX_EPOCHS:
+        break
+    if len(log_likelihood_curve) > 10:
+        diff = ll - log_likelihood_curve[epoch-10]
+        if abs(diff) < EPSILON:
+            break
+    print "  epoch={0:6d}  log likelihood={1:+10.4f}   diff={2:8.4f}".format(epoch,ll,diff)
+    print "prev_sample=",prevalence_sample
+    print "prev_mle=",prevalence_mle
     epoch += 1
 
+# print basics
+print ""
+print "SIZES"
+print "I=",I
+print "J=",J
+print "K=",K
+print "N=",N
+
+
+# print resulting estimates
+print ""
+print "PREVALENCE ESTIMATES"
+print "{0:>2s}, {1:>5s}, {2:>5s}, {3:>5s}, {4:>6s}, {5:>6s}".format("k","sim","samp","MLE","d.sim","d.samp")
 for k in Ks:
-    print "prev*[{0:2d}]={1:5.3f} prev[]={2:5.3f}  delta={3:+5.3f}".format(k,prev[k],prevalence[k],prev[k]-prevalence[k])
+    print "{0:2d}, {1:5.3f}, {2:5.3f}, {3:5.3f}, {4:+5.3f}, {5:+5.3f}".format(k,prevalence[k],prevalence_sample[k],prevalence_mle[k],prevalence_mle[k]-prevalence[k],prevalence_mle[k]-prevalence_sample[k])
+
+print ""
+print "ACCURACY ESTIMATES"
+print "{0:>3s},{1:>2s},{2:>2s}, {3:>5s}, {4:>5s}, {5:>5s}, {6:>6s}, {7:>6s}".format("j","k1","k2","sim","samp","mle","d.sim","d.samp")
 for j in Js:
     for k1 in Ks:
         for k2 in Ks:
-            print "acc*[{0:3d},{1:2d},{2:2d}]={3:5.3f}  acc[]={4:5.3f}  delta={5:+5.3f}".format(j,k1,k2,acc[j][k1][k2],accuracy[j][k1][k2],acc[j][k1][k2]-accuracy[j][k1][k2])
-#for i in Is:
-#    for k in Ks:
-#        print "E[cat[",i,",",k,"]|...]=",cat[i][k]
+            print "{0:3d},{1:2d},{2:2d}, {3:5.3f}, {4:5.3f}, {5:5.3f}, {6:+5.3f}, {7:+5.3f}".format(j,k1,k2,accuracy[j][k1][k2],accuracy_sample[j][k1][k2],accuracy_mle[j][k1][k2],accuracy_mle[j][k1][k2]-accuracy[j][k1][k2],accuracy_mle[j][k1][k2]-accuracy_sample[j][k1][k2])
 
