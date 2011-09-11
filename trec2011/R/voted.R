d3b2f78ce8c5ecb677f6b745fc0eb1af585f5064
carp <carp@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2011-09-11 23:14:17 +0000

    rc on paper

--- trec2011/R/voted.R
+++ trec2011/R/voted.R
@@ -8,6 +8,7 @@ for (i in 1:I1) {
 
 pp <- qplot(z1_voted,z1_post,geom=c("point","smooth"),alpha=I(1/20),
       xlab="voted relevance", ylab="estimated relevance", main="Estimated vs. Voted Relevance");
+pp;
 
 # pdf(file="../paper/img/vote_vs_estimate_2.pdf",width=4,height=4);
 # pp;
