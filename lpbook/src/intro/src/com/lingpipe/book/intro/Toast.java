21b57893ba78444789a20238fe5ddd2a3971f0b0
mitzi <mitzi@d9cdb42f-ec5f-0410-a886-e39e85a9bf9c>
2013-01-13 23:02:11 +0000

    intro demos utf-8

--- lpbook/src/intro/src/com/lingpipe/book/intro/Toast.java
+++ lpbook/src/intro/src/com/lingpipe/book/intro/Toast.java
@@ -7,12 +7,15 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class Toast {
+
+/*x Toast.1*/
     public static String CHEERS = "cheers";
     public static String SANTE = "sant\u00e9";
     public static String SKOL = "sk\u00e5l";
+    public static String GAMBAE = "\uac74\ubc30";
+/*x*/
     public static String NAZDROV = "\u041d\u0430 \u0437\u0434\u043e\u0440\u0432\u044c";
     public static String KANPAI = "\u4e73\u676f";
-    public static String GAMBAE = "\uac74\ubc30";
 
     public static void main(String[] args) 
         throws IOException {
@@ -20,6 +23,7 @@ public class Toast {
         OutputStream outStream = new FileOutputStream("toast.txt");
         OutputStreamWriter outWriter = new OutputStreamWriter(outStream,"UTF-8");
         PrintWriter out = new PrintWriter(outWriter,true);
+	out.println("Before we drink, we say a toast");
 	out.println("in English: " + CHEERS);
 	out.println("in French:  " + SANTE);
 	out.println("in Norwegian: " + SKOL);
